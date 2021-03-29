0f6528ada2b0927751f2a172287f74e793bc6b6e
Joshua Messitte <68798981+joshmess@users.noreply.github.com>
2021-03-28 21:37:51 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,104 @@
+particlesJS.load('particles-js', '../particles.json', function() {});
+
+window.onscroll = function(){
+  if (window.scrollY >= 420){
+    document.getElementById("arrow").setAttribute("style", "display: none");
+  }
+  else{
+    document.getElementById("arrow").setAttribute("style", "display: block");
+  }
+}
+
+let TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    let i = this.loopNum % this.toRotate.length;
+    let fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    let that = this;
+    let delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    let elements = document.getElementsByClassName('typewrite');
+    for (let i=0; i<elements.length; i++) {
+        let toRotate = elements[i].getAttribute('data-type');
+        let period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    let css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #1a1a1a}";
+    document.body.appendChild(css);
+  };
+
+  let modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  let btn = document.getElementById("myBtn");
+  
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks on the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+function showImage(){
+  if (
+    $("#sparc-mac-pic").is(":visible")){
+    $("#sparc-mac-pic").hide();
+    $("#sparc-mac-button").html("Show");
+  }
+  else{
+    $("#sparc-mac-pic").show();
+    $("#sparc-mac-button").html("Hide");
+  }
+}
